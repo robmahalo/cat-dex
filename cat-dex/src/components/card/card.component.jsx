7989923cbe0ec4679c69a26957c7dfb957397e96
Robby Manalo <44278625+robmahalo@users.noreply.github.com>
2021-08-30 14:02:37 -0400

    Change name to cat-dex

--- cat-dex/src/components/card/card.component.jsx
+++ cat-dex/src/components/card/card.component.jsx
@@ -5,6 +5,7 @@ import './card.styles.css';
 export const Card = (props) => (
     <div className='card-container'>
         <img alt="cat" src={`https://robohash.org/${props.cat.id}?set=set4`}/>
-        <h1> {props.cat.name}</h1>
+        <h2> {props.cat.name} </h2>
+        <p> {props.cat.email} </p>
     </div>
 )
